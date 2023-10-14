5259dafe98a88d08c0f3a597ffc83d1297e9876e
VolLove <103765155+VolLove@users.noreply.github.com>
2023-10-14 15:37:25 +0700

    up

--- ProjectLTDD1/app/src/main/java/com/example/myapplication/BunkerActivity.java
+++ ProjectLTDD1/app/src/main/java/com/example/myapplication/BunkerActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ActionMenuView;
+import android.widget.Toolbar;
 
 public class BunkerActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class BunkerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bunker);
+        setControl();
+        setLoad();
+        setEvent();
+    }
+
+    private void setEvent() {
+    }
+
+    private void setLoad() {
+
+    }
+
+    private void setControl() {
     }
 }
--- ProjectLTDD1/app/src/main/java/com/example/myapplication/DetailActivity.java
+++ ProjectLTDD1/app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -3,12 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ActionMenuView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ActionMenuView actionMenuView;
+    MenuItem menuItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+    private void setLoad() {
+
+        menuItem.setIcon(R.drawable.ic_launcher_background);
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+        actionMenuView.requestLayout();
+    }
+
+    private void setControl() {
+        actionMenuView = findViewById(R.id.detailMenu);
+        menuItem = actionMenuView.getMenu().add(Menu.NONE, 1 , Menu.NONE ,"Chỉn sửa");
+        menuItem = actionMenuView.getMenu().add(Menu.NONE, 2 , Menu.NONE ,"Xóa");
     }
 }
