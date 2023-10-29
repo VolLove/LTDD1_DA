36590e382a4236ad4751e76525cd71d7a7b053d6
VolLove <103765155+VolLove@users.noreply.github.com>
2023-10-29 12:02:58 +0700

    update

--- ProjectLTDD1/app/src/main/java/Model/Parcel.java
+++ ProjectLTDD1/app/src/main/java/Model/Parcel.java
@@ -1,54 +1,6 @@
 package Model;
 
 public class Parcel {
-    private Integer id,postage;
-    private String title,decription;
-
-    public Parcel() {
-    }
-
-    public Parcel(Integer id, Integer postage, String title, String decription) {
-        this.id = id;
-        this.postage = postage;
-        this.title = title;
-        this.decription = decription;
-    }
-
-    public Parcel(Integer postage, String title, String decription) {
-        this.postage = postage;
-        this.title = title;
-        this.decription = decription;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPostage() {
-        return postage;
-    }
-
-    public void setPostage(Integer postage) {
-        this.postage = postage;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDecription() {
-        return decription;
-    }
-
-    public void setDecription(String decription) {
-        this.decription = decription;
-    }
+    private Integer id, transport_free,weight,status;
+    private String title, id_type, name_sender, phone_sender, name_receiver, phone_receiver, address_receiver;
 }
